790b9cd2b9612e6f56a4a3d8e91985d4fa8d8138
Ivan Tishkin <Ivan1969@211.ru>
2018-12-10 03:11:59 +0500

    bitrix
    bitrix

--- js/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('anchor').addEventListener('click', function (e) {
-    document.querySelector("#icons").scrollIntoView({
-        behavior: 'smooth' // не работает в сафари, лучший браузер всё таки :(
-    });
-});
