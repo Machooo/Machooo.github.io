1651a7872e91bbd108379edb28ff432d343a40f3
Ivan Tishkin <ivan1969@211.ru>
2019-12-02 13:30:06 +0500

    xakaton
    xakaton

--- mixins/README.md
+++
@@ -1,3 +0,0 @@
-# MIXIN
-
-**This directory is not required, you can delete it if you don't want to use it.**
--- static/sw.js
+++
@@ -1,5 +0,0 @@
-// THIS FILE SHOULD NOT BE VERSION CONTROLLED
-self.addEventListener('activate', async () => {
-  // This will be called only once when the service worker is activated.
-  console.log('service worker activate')
-})
